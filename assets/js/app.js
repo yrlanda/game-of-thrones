8e890ecc85a86c0278d0c89cfc89cf0b56dcf569
Yrlanda <yrlaterivero@gmal.com>
2019-01-20 12:21:31 -0300

    se añade funcion para devolver la lista de las temporadas

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,25 @@
  * Acá va solo la lógica
  * 
  */
+
+const getListofSeasons = (data) => {
+    return data.reduce(function (carry, item) {
+        if (item['seasonNum'] && !~carry.indexOf(item['seasonNum'])) carry.push(item['seasonNum']);
+        return carry;
+    }, []);
+}
+
+window.getListofSeasons = getListofSeasons;
+
+// funcion para filtrar
+const filterSeason = (data, condition) => {
+    if (condition == "all") {
+      return data;
+    }
+    const filteredType = data.filter(element => {
+      return element.seasonNum == condition;
+    })
+    return filteredType;
+  }
+  
+  window.filterSeason = filterSeason;
